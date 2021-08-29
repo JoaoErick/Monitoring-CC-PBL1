5652ce46162dfbcd2677d763529625dd4d82792a
JoaoErick <jerick1415@outlook.com>
2021-08-29 18:09:26 -0300

    First commit.

---
+++ README.md
@@ -0,0 +1,1 @@
+# Monitoring-CC-PBL1
---
+++ src/monitoring/Monitoring.java
@@ -0,0 +1,55 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package monitoring;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.stage.Stage;
+
+/**
+ *
+ * @author Optimus 2020
+ */
+public class Monitoring extends Application {
+    
+    private static Stage stage;
+    
+    @Override
+    public void start(Stage stage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("FXMLDocument.fxml"));
+        
+        Scene scene = new Scene(root);
+        
+        stage.setScene(scene);
+        stage.setTitle("Monitoramento - COVID-19");
+        stage.setResizable(false);
+        stage.show();
+        setStage(stage);
+        
+        Image image = new Image("/image/monitoring-covid-icon.png");
+
+        stage.getIcons().add(image);
+    }
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        launch(args);
+    }
+    
+    public static Stage getStage() {
+        return stage;
+    }
+
+    public static void setStage(Stage stage) {
+        Monitoring.stage = stage;
+    }
+    
+}
